10c2a4a31f21fb5ae9c62cf46dfe330aa718d7a8
mohsinscope <mohsin.ali@scopesky.net>
2024-12-04 17:04:01 +0300

    Added Two Entities off,Gov

--- OMSV1.Domain/Entities/Governorates/Governorate.cs
+++ OMSV1.Domain/Entities/Governorates/Governorate.cs
@@ -1,8 +1,13 @@
 using System;
+using OMSV1.Domain.Entities.Offices;
 
 namespace OMSV1.Domain.Entities.Governorates;
 
-public class Governorate
+public class Governorate(string name, string code)
 {
+    public string Name { get; private set; } = name;
+    public string Code { get; private set; } = code;
+    private List<Office> _offices = new List<Office>();
+    public IReadOnlyCollection<Office> Offices => _offices.AsReadOnly();
 
 }
--- OMSV1.Domain/Entities/Offices/Office.cs
+++ OMSV1.Domain/Entities/Offices/Office.cs
@@ -1,10 +1,27 @@
 using System;
+using OMSV1.Domain.Entities.Governorates;
 using OMSV1.Domain.SeedWork;
 
 namespace OMSV1.Domain.Entities.Offices;
 
-public class Office : Entity
+public class Office(string name,
+                    string code,
+                    string receivingStaff,
+                    string accountStaff,
+                    string printingStaff,
+                    string qualityStaff,
+                    string deliveryStaff,
+                    int GovernorateId) : Entity
 {
-    
+    public string Name { get; private set; } = name;
+    public string Code { get; private set; } = code;
+    public string ReceivingStaff { get; private set; } = receivingStaff;
+    public string AccountStaff { get; private set; } = accountStaff;
+    public string PrintingStaff { get; private set; } = printingStaff;
+    public string QualityStaff { get; private set; } = qualityStaff;
+    public string DeliveryStaff { get; private set; } = deliveryStaff;
+    public int GovernorateId { get; private set; } = GovernorateId;
+    public Governorate? Governorate { get; private set; }
+
 
 }
