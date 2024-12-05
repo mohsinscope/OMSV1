2ada7a5a222ed5dc33e02fe948f9d9f5a67a21a9
Aliyass21 <ali.yaas@scopesky.iq>
2024-12-05 08:40:57 +0300

    Profiles,Auth Test

--- OMSV1.Domain/obj/Release/net8.0/OMSV1.Domain.AssemblyInfo.cs
+++ OMSV1.Domain/obj/Release/net8.0/OMSV1.Domain.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("OMSV1.Domain")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bb566669f73f9f6a5a1d8b9d5f31870b82156f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("OMSV1.Domain")]
