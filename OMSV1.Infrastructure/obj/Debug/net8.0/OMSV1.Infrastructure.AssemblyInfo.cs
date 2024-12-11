5e57d84100fff579de4fa40d619d6206bb29a787
Aliyass21 <ali.yaas@scopesky.iq>
2024-12-11 13:34:04 +0300

    added DTOS TO OFFICES AND GOVERNORATES

--- OMSV1.Infrastructure/obj/Debug/net8.0/OMSV1.Infrastructure.AssemblyInfo.cs
+++ OMSV1.Infrastructure/obj/Debug/net8.0/OMSV1.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OMSV1.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+861807e7c1dc9723d12cf874f7360f6e9a9b6025")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+271f174aeebe7db532e9a743c10291c28ae3dda3")]
 [assembly: System.Reflection.AssemblyProductAttribute("OMSV1.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OMSV1.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
