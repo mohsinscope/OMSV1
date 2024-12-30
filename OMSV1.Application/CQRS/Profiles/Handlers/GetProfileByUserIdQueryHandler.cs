10efbc341d2e4ef5bafbcd4e325a9d6bc7f7dc72
mohsinscope <mohsin.ali@scopesky.net>
2024-12-30 12:24:24 +0300

    Change Account Handler

--- OMSV1.Application/CQRS/Profiles/Handlers/GetProfileByUserIdQueryHandler.cs
+++ OMSV1.Application/CQRS/Profiles/Handlers/GetProfileByUserIdQueryHandler.cs
@@ -4,16 +4,19 @@ using OMSV1.Domain.Entities.Profiles;
 using OMSV1.Application.Helpers;
 using OMSV1.Domain.SeedWork;
 using OMSV1.Application.Queries.Profiles;
+using OMSV1.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 namespace OMSV1.Application.CQRS.Queries.Profiles
 {
     public class GetProfileByUserIdQueryHandler : IRequestHandler<GetProfileByUserIdQuery, ProfileDto>
     {
         private readonly IUnitOfWork _unitOfWork;
-
-        public GetProfileByUserIdQueryHandler(IUnitOfWork unitOfWork)
+        private readonly AppDbContext _context;
+        public GetProfileByUserIdQueryHandler(IUnitOfWork unitOfWork,AppDbContext context)
         {
             _unitOfWork = unitOfWork;
+            _context = context;
         }
 
         public async Task<ProfileDto> Handle(GetProfileByUserIdQuery request, CancellationToken cancellationToken)
@@ -21,8 +24,10 @@ namespace OMSV1.Application.CQRS.Queries.Profiles
             try
             {
                 // Fetch the profile entity with related Governorate and Office
-                var profile = await _unitOfWork.Repository<Profile>()
-                    .GetByIdAsync(request.UserId); // Assuming GetByIdAsync is adjusted to fetch by UserId
+            var profile = await _context.Profiles
+                .Include(p => p.Governorate)
+                .Include(p => p.Office)
+                .FirstOrDefaultAsync(p => p.UserId == request.UserId, cancellationToken);// Assuming GetByIdAsync is adjusted to fetch by UserId
 
                 if (profile == null)
                 {
