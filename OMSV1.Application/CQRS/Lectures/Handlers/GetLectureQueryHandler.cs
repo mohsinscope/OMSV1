a795ff80a7643a18da34d696e01258a3ccdfe5be
Aliyass21 <ali.yaas@scopesky.iq>
2025-01-13 14:33:02 +0300

    Added Filter to lectureTypeIds

--- OMSV1.Application/CQRS/Lectures/Handlers/GetLectureQueryHandler.cs
+++ OMSV1.Application/CQRS/Lectures/Handlers/GetLectureQueryHandler.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using OMSV1.Application.CQRS.Lectures.Queries;
 using OMSV1.Application.Dtos.Lectures;
 using OMSV1.Application.Helpers;
@@ -25,7 +26,7 @@ namespace OMSV1.Application.CQRS.Lectures.Handlers
         {
             try
             {
-                // Create the specification based on the query parameters, including CompanyId and LectureTypeId
+                // Create the specification based on the query parameters
                 var spec = new FilterLecturesSpecification(
                     request.Title,
                     request.StartDate,
@@ -33,17 +34,28 @@ namespace OMSV1.Application.CQRS.Lectures.Handlers
                     request.OfficeId,
                     request.GovernorateId,
                     request.ProfileId,
-                    request.CompanyId, // Passing CompanyId to the specification
-                    request.LectureTypeId // Passing LectureTypeId to the specification
+                    request.CompanyId,
+                    request.LectureTypeIds // Pass the list of LectureTypeIds
                 );
 
                 // Get the queryable list of Lecture entities
-                var queryableResult = _repository.ListAsQueryable(spec);
+                var queryableResult = _repository.ListAsQueryable(spec)
+                    .Include(l => l.LectureLectureTypes) // Include LectureTypes navigation property
+                    .ThenInclude(lt => lt.LectureType); // Include LectureType entity
 
-                // Map to LectureDto
-                var mappedQuery = queryableResult.ProjectTo<LectureAllDto>(_mapper.ConfigurationProvider);
+                // Map to LectureAllDto with LectureTypeNames
+                var mappedQuery = queryableResult.Select(l => new LectureAllDto
+                {
+                    Id = l.Id,
+                    Title = l.Title,
+                    Date = l.Date,
+                    OfficeName = l.Office.Name,
+                    GovernorateName = l.Governorate.Name,
+                    CompanyName = l.Company.Name,
+                    LectureTypeNames = l.LectureLectureTypes.Select(lt => lt.LectureType.Name).ToList()
+                });
 
-                // Create a paginated list of LectureDto
+                // Create a paginated list of LectureAllDto
                 return await PagedList<LectureAllDto>.CreateAsync(mappedQuery, request.PaginationParams.PageNumber, request.PaginationParams.PageSize);
             }
             catch (Exception ex)
@@ -52,5 +64,6 @@ namespace OMSV1.Application.CQRS.Lectures.Handlers
                 throw new HandlerException("An error occurred while fetching lectures.", ex);
             }
         }
+
     }
 }
--- OMSV1.Application/CQRS/Lectures/Queries/GetLectureQuery.cs
+++ OMSV1.Application/CQRS/Lectures/Queries/GetLectureQuery.cs
@@ -1,29 +1,26 @@
-
-
-
 using MediatR;
 using OMSV1.Application.Dtos.Lectures;
 using OMSV1.Application.Helpers;
 
-namespace OMSV1.Application.CQRS.Lectures.Queries;
-public class GetLectureQuery : IRequest <PagedList<LectureAllDto>>
+namespace OMSV1.Application.CQRS.Lectures.Queries
 {
-    public string Title { get; set; }
-    public DateTime? StartDate { get; set; }
-    public DateTime? EndDate { get; set; }
-    public Guid? OfficeId { get; set; }
-    public Guid? GovernorateId { get; set; }
-    public Guid? ProfileId { get; set; }
-    public Guid? CompanyId {get;set;}
-    public Guid? LectureTypeId {get;set;}
-    public int PageNumber { get; set; } = 1;
-    public int PageSize { get; set; } = 10;
-    public PaginationParams PaginationParams { get;set; }
-
-    public GetLectureQuery (PaginationParams paginationParams)
+    public class GetLectureQuery : IRequest<PagedList<LectureAllDto>>
     {
-        PaginationParams = paginationParams;
+        public string Title { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public Guid? OfficeId { get; set; }
+        public Guid? GovernorateId { get; set; }
+        public Guid? ProfileId { get; set; }
+        public Guid? CompanyId { get; set; }
+        public List<Guid>? LectureTypeIds { get; set; } // Changed from Guid? to List<Guid>?
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+        public PaginationParams PaginationParams { get; set; }
+
+        public GetLectureQuery(PaginationParams paginationParams)
+        {
+            PaginationParams = paginationParams;
+        }
     }
-
-
 }
