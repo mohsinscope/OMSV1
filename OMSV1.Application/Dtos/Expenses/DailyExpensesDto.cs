220d119df0a6fac34c32f8c37a520b8d4c69eb1a
Aliyass21 <ali.yaas@scopesky.iq>
2025-01-14 10:29:05 +0300

    Fixed Issue where expesetypename is null

--- OMSV1.Application/Dtos/Expenses/DailyExpensesDto.cs
+++ OMSV1.Application/Dtos/Expenses/DailyExpensesDto.cs
@@ -1,12 +1,15 @@
+using OMSV1.Domain.Entities.Expenses;
+
 namespace OMSV1.Application.DTOs.Expenses;
 
 public class DailyExpensesDto
 {
+    public Guid MonthlyExpensesId { get; set; }
     public Guid Id { get; set; }
     public decimal Price { get; set; }
     public int Quantity { get; set; }
     public decimal Amount { get; set; }
     public string Notes { get; set; }
     public DateTime ExpenseDate { get; set; }
-    public string ExpenseTypeName {get; set;}
+    public string ExpenseTypeName { get; set; } // Include only ExpenseType name
 }
